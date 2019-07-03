0cb03cd561154b5f6299ab831acadc88f321d685
Yashvant Sikarvar <yashvant@bobble.ai>
2019-07-03 17:18:11 +0530

    v12.0

---
+++ BobbleSDKSampleKeyboard/BobbleKeyboardSDK.framework/Headers/BobbleKeyboardSDK.h
@@ -0,0 +1,28 @@
+
+/*
+ *  ____        _     _     _      ___ __  __ _____   ____  ____  _  __
+ * | __ )  ___ | |__ | |__ | | ___|_ _|  \/  | ____| / ___||  _ \| |/ /
+ * |  _ \ / _ \| '_ \| '_ \| |/ _ \| || |\/| |  _|   \___ \| | | | ' /
+ * | |_) | (_) | |_) | |_) | |  __/| || |  | | |___   ___) | |_| | . \
+ * |____/ \___/|_.__/|_.__/|_|\___|___|_|  |_|_____| |____/|____/|_|\_\
+ *
+ * Copyright (C) BobbleKeyboard@2018 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+#import <UIKit/UIKit.h>
+#import "SwipeView.h"
+#import "KRLCollectionViewGridLayout.h"
+#import "UIDevice-Hardware.h"
+#import "BLWordSuggestions.h"
+
+//! Project version number for BobbleKeyboardSDK.
+FOUNDATION_EXPORT double BobbleKeyboardSDKVersionNumber;
+
+//! Project version string for BobbleKeyboardSDK.
+FOUNDATION_EXPORT const unsigned char BobbleKeyboardSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BobbleKeyboardSDK/PublicHeader.h>
+
+
